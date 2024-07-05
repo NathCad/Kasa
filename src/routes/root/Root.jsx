74214e18d0409cba1d0fd4c9bc3da7bba5b21969
NathCad <nathalie.cadet.tb@gmail.com>
2024-07-05 19:43:37 +0200

    Root jsx

--- src/routes/root/Root.jsx
+++ src/routes/root/Root.jsx
@@ -1,7 +1,32 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import Logo from "../../assets/Logo";
+import "./root.scss";
 
 const Root = () => {
-  return <Outlet />;
+  return (
+    <>
+      <header>
+        <Logo />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Accueil</Link>
+            </li>
+            <li>
+              <Link to="/aPropos">A Propos</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <Logo />
+        <small>&copy; 2020 Kasa. All rights reserved</small>
+      </footer>
+    </>
+  );
 };
 export default Root;
