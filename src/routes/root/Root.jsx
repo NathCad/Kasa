0804c8fc3aac6285fff4b4ef220e378e722398b4
NathCad <nathalie.cadet.tb@gmail.com>
2024-07-07 17:07:38 +0200

    root style + active link

--- src/routes/root/Root.jsx
+++ src/routes/root/Root.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import Logo from "../../assets/Logo";
 import "./root.scss";
 
 const Root = () => {
+  const location = useLocation();
+  const isActive = (pathname) =>
+    location.pathname === pathname ? "active" : "";
+
   return (
     <>
       <header>
@@ -11,10 +15,14 @@ const Root = () => {
         <nav>
           <ul>
             <li>
-              <Link to="/">Accueil</Link>
+              <Link className={isActive("/")} to="/">
+                Accueil
+              </Link>
             </li>
             <li>
-              <Link to="/aPropos">A Propos</Link>
+              <Link className={isActive("/aPropos")} to="/aPropos">
+                A Propos
+              </Link>
             </li>
           </ul>
         </nav>
