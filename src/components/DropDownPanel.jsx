f90f6b3cec46027d7df4179c54d7ebb51c0cdf88
NathCad <nathalie.cadet.tb@gmail.com>
2024-07-09 16:27:54 +0200

    A Propos: style + scss

--- src/components/DropDownPanel.jsx
+++ src/components/DropDownPanel.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
-
 import "./dropDownPanel.scss";
+import { Arrow } from "./../assets/Arrow";
 
 const DropDownPanel = ({ title, children }) => {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <article className="drop-down-panel">
+      <article className={`drop-down-panel${open ? " open" : ""}`}>
         <h1>
-          {title} <button onClick={() => setOpen(!open)}>x</button>
+          {title}
+          <button onClick={() => setOpen(!open)}>
+            <Arrow />
+          </button>
         </h1>
-        {open && <div>{children}</div>}
+        <div className="drop-down-panel-content">{children}</div>
       </article>
     </>
   );
