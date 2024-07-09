b09d59c3d1b9ff81f3f64c8969b067fab5e71727
NathCad <nathalie.cadet.tb@gmail.com>
2024-07-09 22:29:56 +0200

    Page Accueil

--- src/routes/accueil/Accueil.jsx
+++ src/routes/accueil/Accueil.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import "./accueil.scss";
 
 const Accueil = () => {
-  return <h1>Accueil</h1>;
+  
+  return <section id="accueil">
+    <h1>Chez vous, partout et ailleurs</h1>
+   
+    <div className="gallery"></div>
+
+  </section>;
 };
 export default Accueil;
